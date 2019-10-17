a0f91e20e8c7daffc1dd5a109af1b3fc90542731
Tad Keller <logischgl@gmail.com>
2019-10-17 13:01:13 -0500

    add submods etc

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/monter/Downloads/primitive
+# Install script for directory: /home/monter/Documents/primitive
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/monter/Downloads/primitive/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/monter/Documents/primitive/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
